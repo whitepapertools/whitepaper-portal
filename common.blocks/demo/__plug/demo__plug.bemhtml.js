086d207f5ebe0508cf7a85d16c7a60ba20a17c8e
Кирилл Э. Федотов <kirillback@yamoney.ru>
2018-06-02 00:57:33 +0300

    create responsive 'hero' block
    additionally, change logo in the header

--- common.blocks/demo/__plug/demo__plug.bemhtml.js
+++ common.blocks/demo/__plug/demo__plug.bemhtml.js
@@ -1,58 +1,56 @@
 block('demo').elem('plug')(
 	content()(function(n, ctx) {
-		return [
-		{
-			elem: 'plug-card',
-			mix: { block: 'pt-card'},
+		return {
+			block: 'hero',
+			mix: { block: 'theme', mods: { color: 'whitepaper-inverse' } },
 			content: [
-			{
-				elem: 'plug-content',
-				mix: { block: 'pt-card', elem: 'content', elemMods: { 'space-a': 'xl' }},
-				content: [
 				{
-					elem: 'plug-illustration',
-					mix: [
-						{ block: 'pt-card', elem: 'image' },
-						{ block: 'decorator', mods: {'indent-b': 'xxl'}}
-					],
-					content: {
-						block: 'image',
-						url: '/assets/i/bear.svg'
-					}
-				},
-				{
-					elem: 'plug-description',
-					mix: { block: 'text', mods: { size: 'xxl', weight: 'bold', align: 'center' }},
-					content: 'Декларативная дизайн-система'
-				}]
-			},
-			{
-				elem: 'plug-action',
-				mix: { block: 'pt-card', elem: 'footer', elemMods: {'space-a': 'xl', 'align': 'between' }},
-				content: [
-				{
-					block: 'button',
-					mods: { theme: 'islands', size: 'xl', view: 'action' },
-					url: 'https://github.com/bemdesign',
-					mix: { block: 'demo', elem: 'plug-button' },
-					text: [
-						{ block: 'text', mods: { style: 'italic', display: 'inline'}, content: 'Visit '},
-						'Github'
+					elem: 'intro',
+					content: [
+						{
+							block: 'image',
+							url: '../../assets/i/wp-logo_hero.svg'
+						},
+						{
+							elem: 'description',
+							content: {
+								block: 'text',
+								mods: { size: 'xxl', view: 'primary', align: 'center' },
+								content: 'Некомпонентная декларативаная открытая дизайн-система'
+							}
+						},
 					]
 				},
 				{
-					elem: 'plug-soclink',
-					elemMods: { icon: 'telegram'},
-					tag: 'a',
-					attrs: { 'href': 'https://t.me/whitepapertools'}
-				},
-				{
-					elem: 'plug-soclink',
-					elemMods: { icon: 'twitter'},
-					tag: 'a',
-					attrs: { 'href': 'https://twitter.com/whitepapertools'}
-				}]
-			}]
-		}]
+					elem: 'contacts',
+					content: [
+						{
+							block: 'text',
+							mods: { view: 'primary', size: 'l', display: 'inline' },
+							content: 'Мы на '
+						},
+						{
+							block: 'text',
+							tag: 'a',
+							attrs: { href: 'https://github.com/whitepapertools', style: 'color: #0070F0;' },
+							mods: { size: 'l', view: 'link', display: 'inline' },
+							content: 'github'
+						},
+						{
+							block: 'text',
+							mods: { view: 'primary', size: 'l', display: 'inline' },
+							content: ' и в '
+						},
+						{
+							block: 'text',
+							tag: 'a',
+							attrs: { href: 'https://t.me/bem_design', style: 'color: #0070F0;' },
+							mods: { size: 'l', view: 'link', display: 'inline' },
+							content: 'telegram'
+						},
+					]
+				}
+			]
+		}
 	})
 );
