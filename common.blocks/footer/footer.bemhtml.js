16f00eb185c4314d41f76960cc1f91be6cb8ea49
Михаил О. Колосков <m.koloskov@live.ru>
2018-06-05 17:27:45 +0300

    update community and event blocks

--- common.blocks/footer/footer.bemhtml.js
+++ common.blocks/footer/footer.bemhtml.js
@@ -17,35 +17,35 @@ block('footer')(
 							mods: { name: 'github', size: 'l', view: 'primary' },
 							mix: { block: 'decorator', mods: { 'indent-l': 'xs' }},
 							tag: 'a',
-							attrs: { href: 'https://github.com/whitepapertools' }
+							attrs: { href: 'https://github.com/whitepapertools', target: '_blank' }
 						},
 						{
 							block: 'social-icon',
 							mods: { name: 'telegram', size: 'l', view: 'primary' },
 							mix: { block: 'decorator', mods: { 'indent-l': 'xs' }},
 							tag: 'a',
-							attrs: { href: 'https://t.me/whitepapertools' }
+							attrs: { href: 'https://t.me/whitepapertools', target: '_blank' }
 						},
 						{
 							block: 'social-icon',
 							mods: { name: 'twitter', size: 'l', view: 'primary' },
 							mix: { block: 'decorator', mods: { 'indent-l': 'xs' }},
 							tag: 'a',
-							attrs: { href: 'https://twitter.com/whitepapertools' }
+							attrs: { href: 'https://twitter.com/whitepapertools', target: '_blank' }
 						},
 						{
 							block: 'social-icon',
 							mods: { name: 'codepen', size: 'l', view: 'primary' },
 							mix: { block: 'decorator', mods: { 'indent-l': 'xs' }},
 							tag: 'a',
-							attrs: { href: 'https://codepen.io/whitepapertools' }
+							attrs: { href: 'https://codepen.io/whitepapertools', target: '_blank' }
 						},
 						{
 							block: 'social-icon',
 							mods: { name: 'dribbble', size: 'l', view: 'primary' },
 							mix: { block: 'decorator', mods: { 'indent-l': 'xs' }},
 							tag: 'a',
-							attrs: { href: 'https://dribbble.com/whitepapertools' }
+							attrs: { href: 'https://dribbble.com/whitepapertools', target: '_blank' }
 						}
 					]
 				}
