94808474cc3fcad344473d0fb1ee9db9b427d1d1
Михаил О. Колосков <m.koloskov@live.ru>
2018-08-27 23:04:35 +0300

    add counter

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1320,6 +1320,41 @@ module.exports = {
 						},
 						/* /Subscribe */
 
+						/* Yandex.Metrika counter */
+						{
+							block: 'count',
+							content: {
+								html: `<script type="text/javascript" >
+								(function (d, w, c) {
+										(w[c] = w[c] || []).push(function() {
+												try {
+														w.yaCounter50119540 = new Ya.Metrika2({
+																id:50119540,
+																clickmap:true,
+																trackLinks:true,
+																accurateTrackBounce:true,
+																webvisor:true
+														});
+												} catch(e) { }
+										});
+						
+										var n = d.getElementsByTagName("script")[0],
+												s = d.createElement("script"),
+												f = function () { n.parentNode.insertBefore(s, n); };
+										s.type = "text/javascript";
+										s.async = true;
+										s.src = "https://mc.yandex.ru/metrika/tag.js";
+						
+										if (w.opera == "[object Opera]") {
+												d.addEventListener("DOMContentLoaded", f, false);
+										} else { f(); }
+								})(document, window, "yandex_metrika_callbacks2");
+						</script>
+						<noscript><div><img src="https://mc.yandex.ru/watch/50119540" style="position:absolute; left:-9999px;" alt="" /></div></noscript>`
+							}
+						},
+						/* /Yandex.Metrika counter */
+
 						/* Footer */
 						{
 							block: 'footer',
