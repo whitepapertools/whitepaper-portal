510d3964ba276e1d3125fefed9839f287f3d7b4c
Вадим А. Матвеев <vamatveev@yamoney.ru>
2018-11-24 20:12:33 +0300

    add space page

--- common.blocks/doc-styles/doc-styles.deps.js
+++ common.blocks/doc-styles/doc-styles.deps.js
@@ -3,7 +3,9 @@
 		shouldDeps: [
 			{ block: 'color' },
 			{ block: 'color', elem: 'image' },
-			{ block: 'tpl-grid' }
+			{ block: 'tpl-grid' },
+			{ block: 'space', elem: 'image' },
+			{ block: 'space-image' }
 		]
 	}
 ])
