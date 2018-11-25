eb4481d8103ed26296b7b1114bf65781f37a7f13
Никита Высотский <vysotskii@yamoney.ru>
2018-11-25 15:58:23 +0300

    merge with foooooontic

--- common.blocks/doc-styles/doc-styles.deps.js
+++ common.blocks/doc-styles/doc-styles.deps.js
@@ -6,7 +6,7 @@
 			{ block: 'doc-view' },
 			{ block: 'badge', mods: { view: ['default', 'alert', 'warning', 'success'] } },
 			{ block: 'icon' },
-			{ block: 'icon', mods: { name: 'close', size: 's', view: ['ghost', 'link'] } }
+			{ block: 'icon', mods: { name: 'close', size: 's', view: ['ghost', 'link'] } },
 			{ block: 'tpl-grid' },
 			{ block: 'space', elem: 'image' },
 			{ block: 'space-image' }
