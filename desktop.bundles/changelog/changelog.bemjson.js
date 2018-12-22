e509f50c93e8fba693d0097bac6147831f14e59b
Ilya Kolebayev <kolebayev@gmail.com>
2018-12-22 12:22:46 +0300

    Create changelog.bemjson.js
    Changelog.bemjson.js file added

---
+++ desktop.bundles/changelog/changelog.bemjson.js
@@ -0,0 +1,22 @@
+module.exports = {
+	block: 'page',
+	title: 'Цвета',
+	favicon: '/favicon.ico',
+	lang: 'ru',
+	head: [
+		{ elem: 'meta', attrs: { name: 'description', content: '' } },
+		{ elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
+		{ elem: 'css', url: 'color.min.css' },
+		{ elem: 'css', url: '/assets/bem-grid/bem-grid-l.css' },
+		{ elem: 'js', url: 'https://code.jquery.com/jquery-3.2.1.js' },
+	],
+	scripts: [{ elem: 'js', url: 'color.min.js' }],
+	mods: { theme: 'islands' },
+	mix: {
+		block: 'theme',
+		mods: { color: 'whitepaper-default', space: 'default', breakpoint: 'default', size: 'default', gap: 'small', menu: 'default', font: 'museo' }
+	},
+	content: [
+		
+	]
+};
