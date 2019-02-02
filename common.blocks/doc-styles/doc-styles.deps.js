b92d3ef0e9a3d610ee200fe21ea8017ac6eb15b4
Вадим А. Матвеев <vamatveev@yamoney.ru>
2019-02-02 17:40:40 +0300

    solve merge conflict

--- common.blocks/doc-styles/doc-styles.deps.js
+++ common.blocks/doc-styles/doc-styles.deps.js
@@ -1,11 +1,20 @@
 ([
-			{ block: 'tpl-grid' },
-			{ block: 'tpl-grid', mods: {'row-gap': 'third'} },
-			{ block: 'space', elem: 'image' },
-			{ block: 'space-image' },
-			{ block: 'preview' }
-		]
-	}
+ {
+  shouldDeps: [
+   { block: 'color' },
+   { block: 'color', elem: 'image' },
+   { block: 'doc-view' },
+   { block: 'badge', mods: { view: ['default', 'alert', 'warning', 'success'] } },
+   { block: 'icon' },
+   { block: 'icon', mods: { name: 'close', size: 's', view: ['ghost', 'link'] } },
+   { block: 'tpl-grid' },
+   { block: 'tpl-grid', mods: {'row-gap': 'third'} },
+   { block: 'space', elem: 'image' },
+   { block: 'space-image' },
+   { block: 'button', mods: { theme: 'islands', size: ['xl', 'm'], type: 'link', view: 'action' } },
+   { block: 'button', elem: 'text' },
+   { block: 'component', mods: { whitepaper: ['default', 'success'] } },
+   { block: 'card-doc-preview' },
       { block: 'preview' },
       { block: 'input', mods: { theme: 'islands', width: 'available', size: 'm' } },
       { block: 'input', elem: 'box' },
