cac811215b90252911b58151cbd2acf827a1e618
Кирилл Э. Федотов <kirillback@yamoney.ru>
2019-02-02 18:23:35 +0300

    update index

--- desktop.bundles/new-index/new-index.bemjson.js
+++ desktop.bundles/new-index/new-index.bemjson.js
@@ -167,7 +167,7 @@ module.exports = {
 				/* Demo */
 				{
 					elem: 'content',
-					attrs: { style: 'height: 900px' },
+					attrs: { style: 'height: 400px' },
 					content: {
 						elem: 'container',
 						elemMods: { align: 'center', size: 'm' },
@@ -178,22 +178,530 @@ module.exports = {
 				},
 				/* /Demo */
 
-				/* About */
+				/* Comparing */
 				{
 					elem: 'content',
+					mix: { block: 'decorator', mods: { 'indent-b': 'xxxxxxl' } },
 					content: {
 						elem: 'container',
 						elemMods: { align: 'center', size: 'm' },
 						content: [
-
+							{
+								block: 'text',
+								mix: { block: 'decorator', mods: { 'indent-b': 'xl' } },
+								mods: { size: 'xxxl', view: 'primary', weight: 'bold', },
+								content: 'Как это работает'
+							},
+							{
+								block: 'text',
+								mods: { size: 'l', view: 'secondary', },
+								mix: { block: 'decorator', mods: { 'indent-b': 'xxxxl' } },
+								content: 'Продакшн рэди лоджик из паблик эвйлибл андер эн оупэн сорс лайсэнс.'
+							},
+							{
+								block: 'pt-list',
+								mix: { block: 'decorator', mods: { 'indent-b': 'l' } },
+								elem: 'item',
+								elemMods: { distribute: 'default', 'vertical-align': 'center' },
+								content: [
+									{
+										block: 'text',
+										attrs: { style: 'width: 240px; margin-right: 20px;' },
+										mods: { size: 'l', view: 'secondary' },
+										content: 'whitepaper.tools'
+									},
+									{
+										block: 'steps',
+										content: [
+											{
+												block: 'step-badge',
+												mix: { block: 'theme', mods: { color: 'whitepaper-brand' } },
+												mods: { view: 'wp' },
+												content: 'Дизайн-система'
+											},
+											{
+												block: 'step-badge',
+												mix: { block: 'theme', mods: { color: 'whitepaper-brand' } },
+												mods: { view: 'wp' },
+												content: 'Фронтенд фреймворк'
+											}
+										]
+									},
+									{
+										block: 'text',
+										mods: { size: 'l', view: 'secondary' },
+										content: 'Готово'
+									}
+								]
+							},
+							{
+								block: 'pt-list',
+								elem: 'item',
+								elemMods: { distribute: 'default', 'vertical-align': 'center' },
+								content: [
+									{
+										block: 'text',
+										attrs: { style: 'width: 240px; margin-right: 20px;' },
+										mods: { size: 'l', view: 'secondary' },
+										content: 'Если стирать обычным порошком'
+									},
+									{
+										block: 'steps',
+										content: [
+											{
+												block: 'step-badge',
+												mods: { view: 'other' },
+												content: 'Прототип'
+											},
+											{
+												block: 'step-badge',
+												mods: { view: 'other' },
+												content: 'Дизайн'
+											},
+											{
+												block: 'step-badge',
+												mods: { view: 'other' },
+												content: 'HTML & CSS'
+											},
+											{
+												block: 'step-badge',
+												mods: { view: 'other' },
+												content: 'Фронтенд фреймворк'
+											}
+										]
+									},
+									{
+										block: 'text',
+										mods: { size: 'l', view: 'secondary' },
+										content: 'Готово'
+									}
+								]
+							}
 						]
 					}
 				},
-				/* /About */
-
-				/* Comparing */
-
 				/* /Comparing */
+
+				/* FAQ */
+				{
+					elem: 'content',
+					mix: { block: 'decorator', mods: { 'indent-b': 'xxxxxxl' } },
+					content: {
+						elem: 'container',
+						elemMods: { align: 'center', size: 'm' },
+						content: [
+							{
+								block: 'text',
+								mix: { block: 'decorator', mods: { 'indent-b': 'xl' } },
+								mods: { size: 'xxxl', view: 'primary', weight: 'bold', },
+								content: 'Faq'
+							},
+							{
+								block: 'tpl-grid',
+								mods: { ratio: '1-1-1', 'col-gap': 'full', 'row-gap': 'half' },
+								content: [
+									{
+										block: 'text',
+										mods: { size: 'l', view: 'secondary' },
+										content: '- I am new to coding,is WPT for me?'
+									},
+									{
+										block: 'text',
+										mods: { size: 'l', view: 'secondary' },
+										content: '- I am new to coding,is WPT for me?'
+									},
+									{
+										block: 'text',
+										mods: { size: 'l', view: 'secondary' },
+										content: '- I am new to coding,is WPT for me?'
+									},
+									{
+										block: 'text',
+										mods: { size: 'l', view: 'secondary' },
+										content: '- I am new to coding,is WPT for me?'
+									},
+									{
+										block: 'text',
+										mods: { size: 'l', view: 'secondary' },
+										content: '- I am new to coding,is WPT for me?'
+									},
+									{
+										block: 'text',
+										mods: { size: 'l', view: 'secondary' },
+										content: '- I am new to coding,is WPT for me?'
+									}
+								]
+							}
+						]
+					}
+				},
+				/* /FAQ */
+
+				/* Community / Events */
+				{
+					elem: 'content',
+					mix: { block: 'decorator', mods: { 'indent-b': 'xxxxxxl' } },
+					content: {
+						elem: 'container',
+						elemMods: { align: 'center', size: 'm' },
+						content: [
+							{
+								block: 'tpl-grid',
+								mods: { ratio: '1-1' },
+								content: [
+									{
+										block: 'community',
+										content: [
+											{
+												block: 'text',
+												mix: { block: 'decorator', mods: { 'indent-b': 'xl' } },
+												mods: { size: 'xxxl', view: 'primary', weight: 'bold', },
+												content: 'Сообщество'
+											},
+											{
+												block: 'pt-list',
+												content: [
+													{
+														elem: 'item',
+														elemMods: { distribute: 'between', 'vertical-align': 'center', border: 'bottom', 'space-v': 'xl' },
+														content: [
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'secondary' },
+																content: 'Blog'
+															},
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'secondary' },
+																content: 'Medium'
+															}
+														]
+													},
+													{
+														elem: 'item',
+														elemMods: { distribute: 'between', 'vertical-align': 'center', border: 'bottom', 'space-v': 'xl' },
+														content: [
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'secondary' },
+																content: 'GitHub Tasks'
+															},
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'secondary' },
+																content: '26 Tasks'
+															}
+														]
+													},
+													{
+														elem: 'item',
+														elemMods: { distribute: 'between', 'vertical-align': 'center', border: 'bottom', 'space-v': 'xl' },
+														content: [
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'secondary' },
+																content: 'Telegram'
+															},
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'secondary' },
+																content: '@whitepapertools'
+															}
+														]
+													},
+													{
+														elem: 'item',
+														elemMods: { distribute: 'between', 'vertical-align': 'center', border: 'bottom', 'space-v': 'xl' },
+														content: [
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'secondary' },
+																content: 'Codepen'
+															},
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'secondary' },
+																content: '60 examples '
+															}
+														]
+													},
+													{
+														elem: 'item',
+														elemMods: { distribute: 'between', 'vertical-align': 'center', border: 'bottom', 'space-v': 'xl' },
+														content: [
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'secondary' },
+																content: 'Twitter'
+															},
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'secondary' },
+																content: '@whitepapertools'
+															}
+														]
+													}
+												]
+											}
+										]
+									},
+									{
+										block: 'events',
+										mix: { block: 'theme', mods: { color: 'whitepaper-brand' } },
+										content: [
+											{
+												block: 'text',
+												mix: { block: 'decorator', mods: { 'indent-b': 'xl' } },
+												mods: { size: 'xxxl', view: 'primary', weight: 'bold', },
+												content: 'События'
+											},
+											{
+												block: 'pt-list',
+												content: [
+													{
+														elem: 'item',
+														elemMods: { distribute: 'between', 'vertical-align': 'center', border: 'bottom', 'space-v': 'xl' },
+														content: [
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'primary' },
+																content: 'Dribbble Meetup, 2018'
+															},
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'primary' },
+																content: 'Санкт-Петербург'
+															}
+														]
+													},
+													{
+														elem: 'item',
+														elemMods: { distribute: 'between', 'vertical-align': 'center', border: 'bottom', 'space-v': 'xl' },
+														content: [
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'primary' },
+																content: 'Web Standarts, 2018'
+															},
+															{
+																block: 'text',
+																mods: { size: 'l', view: 'primary' },
+																content: 'Москва'
+															}
+														]
+													},
+												]
+											}
+										]
+									}
+								]
+							}
+						]
+					}
+				},
+				/* Community / Events */
+
+				/* Contribution / Learning */
+				{
+					elem: 'content',
+					mix: { block: 'decorator', mods: { 'indent-b': 'xxxxxxl' } },
+					content: {
+						elem: 'container',
+						elemMods: { align: 'center', size: 'm' },
+						content: [
+							{
+								block: 'tpl-grid',
+								mods: { ratio: '1-1', 'col-gap': 'full' },
+								content: [
+									{
+										block: 'contribution',
+										content: [
+											{
+												block: 'text',
+												mix: { block: 'decorator', mods: { 'indent-b': 'xl' } },
+												mods: { size: 'xxxl', view: 'primary', weight: 'bold', },
+												content: 'Контребьютеры'
+											},
+											{
+												block: 'avatars',
+												mix: { block: 'decorator', mods: { 'indent-b': 'xl' } },
+												content: [
+													{
+														block: 'avatar',
+														content: [
+															{
+																elem: 'photo',
+																elemMods: { size: 's' },
+																content: [
+																]
+															}
+														]
+													},
+													{
+														block: 'avatar',
+														content: [
+															{
+																elem: 'photo',
+																elemMods: { size: 's' },
+																content: [
+																]
+															}
+														]
+													},
+													{
+														block: 'avatar',
+														content: [
+															{
+																elem: 'photo',
+																elemMods: { size: 's' },
+																content: [
+																]
+															}
+														]
+													},
+													{
+														block: 'avatar',
+														content: [
+															{
+																elem: 'photo',
+																elemMods: { size: 's' },
+																content: [
+																]
+															}
+														]
+													},
+													{
+														block: 'avatar',
+														content: [
+															{
+																elem: 'photo',
+																elemMods: { size: 's' },
+																content: [
+																]
+															}
+														]
+													},
+													{
+														block: 'avatar',
+														content: [
+															{
+																elem: 'photo',
+																elemMods: { size: 's' },
+																content: [
+																]
+															}
+														]
+													},
+													{
+														block: 'avatar',
+														content: [
+															{
+																elem: 'photo',
+																elemMods: { size: 's' },
+																content: [
+																]
+															}
+														]
+													},
+													{
+														block: 'avatar',
+														content: [
+															{
+																elem: 'photo',
+																elemMods: { size: 's' },
+																content: [
+																]
+															}
+														]
+													},
+													{
+														block: 'avatar',
+														content: [
+															{
+																elem: 'photo',
+																elemMods: { size: 's' },
+																content: [
+																]
+															}
+														]
+													}
+												]
+											},
+											{
+												block: 'text',
+												mods: { size: 'l', view: 'primary', },
+												content: 'Production ready logic is publicly available under an open source license. This means that the software library and its '
+											},
+											{
+												block: 'doc',
+												content: 'GitHub'
+											}
+										]
+									},
+									{
+										block: 'contribution',
+										content: [
+											{
+												block: 'text',
+												mix: { block: 'decorator', mods: { 'indent-b': 'xl' } },
+												mods: { size: 'xxxl', view: 'primary', weight: 'bold', },
+												content: 'Обучение'
+											},
+											{
+												block: 'text',
+												mods: { size: 'l', view: 'primary', },
+												content: 'Production ready logic is publicly available under an open source license. This means that the software library and its '
+											},
+											{
+												block: 'doc',
+												content: 'Документация'
+											}
+										]
+									}
+								]
+							}
+						]
+					}
+				},
+				/* Contribution / Learning */
+
+				/* Start */
+				{
+					elem: 'content',
+					mix: { block: 'decorator', mods: { 'indent-b': 'xxxxxxl' } },
+					content: {
+						elem: 'container',
+						elemMods: { align: 'center', size: 'm' },
+						content: [
+							{
+								block: 'text',
+								mix: { block: 'decorator', mods: { 'indent-b': 'xl' } },
+								mods: { size: 'xxxl', view: 'primary', weight: 'bold', },
+								content: 'Старт'
+							},
+						]
+					}
+				},
+				/* /Start */
+
+				/* Partners */
+				{
+					elem: 'content',
+					mix: { block: 'decorator', mods: { 'indent-b': 'xxxxxxl' } },
+					content: {
+						elem: 'container',
+						elemMods: { align: 'center', size: 'm' },
+						content: [
+							{
+								block: 'text',
+								mix: { block: 'decorator', mods: { 'indent-b': 'xl' } },
+								mods: { size: 'xxxl', view: 'primary', weight: 'bold', },
+								content: 'Партнеры'
+							},
+						]
+					}
+				},
+				/* /Partners */
 
 				/* Yandex.Metrika counter */
 				{
