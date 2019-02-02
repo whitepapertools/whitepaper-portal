0fa58989a00fa2f3398487a96dc979c7144e9d59
Никита В. Высотский <vysotskii@yamoney.ru>
2019-02-02 17:36:14 +0300

    pt-informer, pt-table

--- common.blocks/doc-styles/doc-styles.deps.js
+++ common.blocks/doc-styles/doc-styles.deps.js
@@ -8,8 +8,16 @@
 			{ block: 'icon' },
 			{ block: 'icon', mods: { name: 'close', size: 's', view: ['ghost', 'link'] } },
 			{ block: 'tpl-grid' },
+			{ block: 'tpl-grid', mods: {'row-gap': 'third'} },
 			{ block: 'space', elem: 'image' },
-			{ block: 'space-image' }
+			{ block: 'space-image' },
+			{ block: 'button', mods: { theme: 'islands', size: ['xl', 'm'], type: 'link', view: 'action' } },
+			{ block: 'button', elem: 'text' },
+			{ block: 'component', mods: { whitepaper: ['default', 'success'] } },
+			{ block: 'card-doc-preview' },
+      { block: 'preview' },
+      { block: 'input', mods: { theme: 'islands', width: 'available', size: 'm' } },
+      { block: 'input', elem: 'box' },
 		]
 	}
 ])
