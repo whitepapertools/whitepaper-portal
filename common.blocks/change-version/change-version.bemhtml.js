36b0069424a27e5d2992877acd2557ce9322c60f
Ilya Kolebayev <kolebayev@gmail.com>
2019-03-03 15:54:43 +0300

    Changelog improved

--- common.blocks/change-version/change-version.bemhtml.js
+++ common.blocks/change-version/change-version.bemhtml.js
@@ -1,68 +1,35 @@
-// block('change-version')(
-// 	content()(function(n, ctx) {
-// 		return [
-// 			{
-// 				elem: 'plug'
-// 			},
-// 			ctx.content
-// 		]
-// 	})
-// );
-
-//block('change-version')(
-  //tag()('a') // Устанавливаем тег menu для блока меню
-
-
-    // content()(function(n, ctx) {
-    //     return {
-    //         elem : 'photo',
-    //         url: ctx.url,
-    //         title: 'john-photo'
-    //     }
-    // }),
-//);
-
-// block('change-version')(
-//     {
-//         // prependContent: '«',
-//         // appendContent: '»'
-//     },
-//     {
-//         appendContent: () => ({ block: 'link' })
-//     }
-// );
-
 block('change-version')(
   content()(function(n, ctx) {
       return {
         // {
           block: 'tpl-grid',
           mods: { columns: '12', 'col-gap': 'two-thirds' },
-          mix: { block: 'decorator', mods: {'indent-b': 'xxxxxl'}},
+          mix: { block: 'decorator', mods: { 'indent-b': 'xxxxxl' }},
           content: [
             // левая колонка
             {
               elem: 'fraction',
-              elemMods: {col: '5'},
+              elemMods: { col: '5' },
               content: [
                 {
                   block: 'version-number',
-                  mix: { block: 'text', mods: {size: 'xxxxl', weight: 'bold' }},
+                  mix: { block: 'text', mods: { size: 'xxxl', weight: 'bold', view: 'primary' }},
+                  // attrs: { 'href':'#${this.ctx.number}' },
                   content: this.ctx.number
                 },
                 {
                   block: 'version-description',
                   mix: [
-                    { block: 'text', mods: {size: 'l'}},
-                    { block:'decorator', mods:{'space-t': 'm'}},
+                    { block: 'text', mods: { size: 'l', view: 'secondary' }},
+                    { block: 'decorator', mods: { 'space-t': 'm' }},
                   ],
                   content: this.ctx.description
                 },
                 {
                   block: 'version-date',
                   mix: [
-                    { block: 'text', mods: {size: 'm', view: 'secondary'}},
-                    { block: 'decorator', mods:{'space-t': 'xxs'}},
+                    { block: 'text', mods: { size: 'm', view: 'ghost' }},
+                    { block: 'decorator', mods: { 'space-t': 's' }},
                   ],
                   content: this.ctx.date
                 },
@@ -74,42 +41,245 @@ block('change-version')(
               elemMods: {col: '7'},
               content: [
                 {
-                  block: 'info-block-margin',
-                  mix: { block: 'decorator', mods: {'indent-l': 'l'}},
+                  block: 'changes-description',
+                  // mix: { block: 'decorator', mods: { 'indent-l': 'm', 'space-a': 'xl' }},
                   content: [
-                    // мажоры
+                    // мажоры: Важные изменения
                     {
-                      block: 'text',
-                      mods: { size: 'xl', view: 'primary' },
-                      content: this.ctx.major
-                    },
-                    {
-                      block: 'pt-list',
-                      mods: { border: 'all', view: 'default' },
+                      elem: 'major-changes',
+                      mix: { block:'decorator', mods: { 'space-b': 'l' }},
                       content: [
                         {
-                            elem: 'item',
-                            elemMods: { 'space-a': 'xl' },
-                            content: this.ctx.major1
+                          block: 'text',
+                          mods: { size: 'xl', view: 'primary' },
+                          mix: [
+                            { block: 'decorator', mods: { 'indent-t': 'xs' }},
+                            { block: 'decorator', mods: { 'indent-b': 'm' }},
+                          ],
+                          content: this.ctx.major
                         },
                         {
-                            elem: 'item',
-                            elemMods: { 'space-a': 'xl' },
-                            content: this.ctx.major2
-                        },
-                        {
-                            elem: 'item',
-                            elemMods: { 'space-a': 'xl' },
-                            content: this.ctx.major3
+                          block: 'pt-list',
+                          // mods: { border: 'all', view: 'default' },
+                          content: [
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  mix: { block:'text', mods: { view:'secondary' }},
+                                  content: this.ctx.major1
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view:'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.major2
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view:'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.major3
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view:'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.major4
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view:'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.major5
+                                }
+                              ]
+                            }
+                          ]
                         }
                       ]
-                    }
+                    },
+                    // миноры: Изменения
+                    {
+                      elem: 'minor',
+                      mix: { block:'decorator', mods: { 'space-b': 'l' }},
+                      content: [
+                        {
+                          block: 'text',
+                          mods: { size: 'xl', view: 'primary' },
+                          mix: [
+                            { block: 'decorator', mods: { 'indent-t': 'xs' }},
+                            { block: 'decorator', mods: { 'indent-b': 'm' }},
+                          ],
+                          content: this.ctx.minor
+                        },
+                        {
+                          block: 'pt-list',
+                          // mods: { border: 'all', view: 'default' },
+                          content: [
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  mix: { block:'text', mods: { view:'secondary' }},
+                                  content: this.ctx.minor1
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view: 'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.minor2
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view: 'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.minor3
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view: 'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.minor4
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view: 'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.minor5
+                                }
+                              ]
+                            },
+                          ]
+                        }
+                      ]
+                    },
+                    // патчи: Багфиксы
+                    {
+                      elem: 'patch',
+                      mix: { block:'decorator', mods: { 'space-b': 'l' }},
+                      content: [
+                        {
+                          block: 'text',
+                          mods: { size: 'xl', view: 'primary' },
+                          mix: [
+                            { block: 'decorator', mods: { 'indent-t': 'xs' }},
+                            { block: 'decorator', mods: { 'indent-b': 'm' }},
+                          ],
+                          content: this.ctx.patch
+                        },
+                        {
+                          block: 'pt-list',
+                          // mods: { border: 'all', view: 'default' },
+                          content: [
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  mix: { block:'text', mods: { view:'secondary' }},
+                                  content: this.ctx.patch1
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view:'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.patch2
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view:'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.patch3
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view:'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.patch4
+                                }
+                              ]
+                            },
+                            {
+                              elem: 'item',
+                              elemMods: { 'space-b': 'm' },
+                              mix: { block:'text', mods: { view:'secondary' }},
+                              content: [
+                                {
+                                  block: 'change-item',
+                                  content: this.ctx.patch5
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    },
                   ]
                 }
               ]
             },
           ]
-        // }
       }
   }),
 );
